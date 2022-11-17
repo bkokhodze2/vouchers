6d6a786830ecef843beb842e753da0175daff44c
bakurikokhodze <b_kokhodze2@cu.edu.ge>
2022-11-17 17:23:54 +0400

    next config git ignore

--- next.config.js
+++ next.config.js
@@ -16,10 +16,10 @@ const nextConfig = {
 	},
 	env: {
 		// for local
-		baseApi: "http://accounting.pirveli.ge/api/racoon-transactions",
+		//baseApi: "http://accounting.pirveli.ge/api/racoon-transactions",
 
 		// for server
-		//baseApi: "https://vouchers.pirveli.ge/api/racoon-transactions",
+		baseApi: "https://vouchers.pirveli.ge/api/racoon-transactions",
 
 	}
 }
